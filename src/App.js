1d264c8f9072fea6f50b683fb077b509ec063546
IKRAIYAN <ihkraiyan@gmail.com>
2023-10-04 00:07:54 +0600

    custom cursor

--- src/App.js
+++ src/App.js
@@ -6,20 +6,36 @@ import NotFound from "./pages/notfound/NotFound"
 import Products from "./pages/products/Products"
 import Navbar from "./components/Navbar"
 import Footer from "./components/Footer"
+import AnimatedCursor from "react-animated-cursor";
+
 const App = () => {
     return (
-        <BrowserRouter>
-            <Navbar />
-            <Routes>
-                <Route index element={<Home />} />
-                <Route path="about" element={<About />}></Route>
-                <Route path="contact" element={<Contact />}></Route>
-                <Route path="menu" element={<Products />}></Route>
-                <Route path="*" element={ <NotFound />}></Route>
-            </Routes>
-            <Footer />
-        </BrowserRouter>
-    )
+      <BrowserRouter>
+        <AnimatedCursor
+          innerSize={8}
+          outerSize={35}
+          innerScale={1}
+          outerScale={2}
+          outerAlpha={0}
+          hasBlendMode={true}
+          innerStyle={{
+            backgroundColor: "#fff",
+          }}
+          outerStyle={{
+            border: "3px solid #fff",
+          }}
+        />
+        <Navbar />
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />}></Route>
+          <Route path="contact" element={<Contact />}></Route>
+          <Route path="menu" element={<Products />}></Route>
+          <Route path="*" element={<NotFound />}></Route>
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    );
 }
 
 export default App
